a31422e6e16e9c929b4aa3bc1c153cdf7b81c2f6
sashi0034 <satoshi3104.nishi@gmail.com>
2022-07-09 15:18:22 +0900

    かんせい

---
+++ ImageViewWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace IllustViewer
+{
+    /// <summary>
+    /// Window1.xaml の相互作用ロジック
+    /// </summary>
+    public partial class ImageViewWindow : Window
+    {
+        private bool isWindowStyleBorderless = false;
+
+        public ImageViewWindow(Size windowSize)
+        {
+            InitializeComponent();
+            this.Width = windowSize.Width;
+            this.Height = windowSize.Height;
+            setWindowStyle(isWindowStyleBorderless);
+        }
+
+        public void TryLoadImage(string imagePath)
+        {
+            imageView.Source = new BitmapImage(new Uri(imagePath));
+        }
+
+
+        internal void TryLoadImage(BitmapSource src)
+        {
+            imageView.Source = src;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {}
+
+
+
+        private void imageView_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            double deltaScale = 1;
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                deltaScale *= 1.2;
+            }
+            if (e.RightButton == MouseButtonState.Pressed)
+            {
+                deltaScale *= 0.8;
+            }
+            imageView.Width = imageView.ActualWidth * deltaScale;
+            imageView.Height = imageView.ActualHeight * deltaScale;
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.F11)
+            {
+                isWindowStyleBorderless = !isWindowStyleBorderless;
+
+                setWindowStyle(isWindowStyleBorderless);
+            }
+        }
+
+
+        private void setWindowStyle(bool isBorederless)
+        {
+            if (isBorederless)
+            {
+                this.WindowStyle = WindowStyle.None;
+            }
+            else
+            {
+                this.WindowStyle = WindowStyle.SingleBorderWindow;
+            }
+        }
+
+        private void ScrollViewer_MouseEnter(object sender, MouseEventArgs e)
+        {
+            scrollViewr.HorizontalScrollBarVisibility = ScrollBarVisibility.Visible;
+            scrollViewr.VerticalScrollBarVisibility = ScrollBarVisibility.Visible;
+        }
+
+        private void ScrollViewer_MouseLeave(object sender, MouseEventArgs e)
+        {
+            scrollViewr.HorizontalScrollBarVisibility = ScrollBarVisibility.Hidden;
+            scrollViewr.VerticalScrollBarVisibility = ScrollBarVisibility.Hidden;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IllustViewer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void grid_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                var fileNames = (string[])e.Data.GetData(DataFormats.FileDrop);
+                foreach (var name in fileNames)
+                {
+                    Debug.Print(name);
+
+                    var window = new ImageViewWindow(new Size(this.Width, this.Height));
+                    try
+                    {
+                        window.TryLoadImage(name);
+                        window.Show();
+                    }
+                    catch (Exception ex)
+                    {
+                        window.Close();
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+                
+            }
+        }
+
+
+        private void loadFromClipboadButton_Click(object sender, RoutedEventArgs e)
+        {
+            var window = new ImageViewWindow(new Size(this.Width, this.Height));
+            try
+            {
+                var src = GetClipboardBitmapDIB();
+                if (src == null) throw new Exception("クリップボードから画像を読み取れませんでした。");
+                window.TryLoadImage(src);
+                window.Show();
+            }
+            catch (Exception ex)
+            {
+                window?.Close();
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+
+        // 参考: https://gogowaten.hatenablog.com/entry/2019/11/12/201852
+        // Autohr: gogowaten
+        private BitmapSource GetClipboardBitmapDIB()
+        {
+            var data = Clipboard.GetDataObject();
+            if (data == null) return null;
+
+            var ms = data.GetData("DeviceIndependentBitmap") as System.IO.MemoryStream;
+            if (ms == null) return null;
+
+            //DeviceIndependentBitmapのbyte配列の15番目がbpp、
+            //これが32未満ならBgr32へ変換、これでアルファの値が0でも255扱いになって表示される
+            byte[] dib = ms.ToArray();
+            if (dib[14] < 32)
+            {
+                return new FormatConvertedBitmap(Clipboard.GetImage(), PixelFormats.Bgr32, null, 0);
+            }
+            else
+            {
+                return Clipboard.GetImage();
+            }
+        }
+
+    }
+}
